f39a528538430e962f5d0835daa63580906a7fd3
hansanhha <kwixs35@gmail.com>
2023-09-22 19:37:51 +0900

    initial commit

---
+++ src/main/java/study/security/this_will_protect_you/ThisWillProtectYouApplication.java
@@ -0,0 +1,13 @@
+package study.security.this_will_protect_you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisWillProtectYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThisWillProtectYouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/security/this_will_protect_you/ThisWillProtectYouApplicationTests.java
@@ -0,0 +1,13 @@
+package study.security.this_will_protect_you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisWillProtectYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
